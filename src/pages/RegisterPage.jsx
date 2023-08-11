12b189b1a64777a888bda19a506b4b51e269a1f0
Ritesh <ritesh.mvteams@gmail.com>
2023-08-11 12:36:09 +0530

    ui: improved register page ui

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container, Table } from "react-bootstrap";
 
 export default function RegisterPage() {
   const [loading, setLoading] = useState(false);
@@ -28,23 +28,46 @@ export default function RegisterPage() {
 
   return (
     <>
-      <h2 className="text-center mt-5">Get Username and password for login</h2>
-      <div className="text-center mt-3">
-        <Button onClick={handleFetchUsers}>Get Username and password</Button>
-      </div>
+      <Container>
+        <h2 className="text-center mt-5">
+          Get Username and password for login
+        </h2>
+        <div className="text-center mt-3">
+          <Button onClick={handleFetchUsers}>Get Username and password</Button>
+        </div>
 
-      <main>
-        {loading && <h2 className="text-center mt-5">Loading...</h2>}
-        {error && <h2 className="text-center mt-5">{error}</h2>}
-        {users?.map((user) => {
-          return (
-            <div key={user.id} className="text-center mt-5">
-              <h4>Username: {user.username}</h4>
-              <h4>Password: {user.password}</h4>
-            </div>
-          );
-        })}
-      </main>
+        <main>
+          {loading && <h2 className="text-center mt-5">Loading...</h2>}
+          {error && <h2 className="text-center mt-5">{error}</h2>}
+
+          {users.length > 0 && (
+            <Table striped bordered hover className="mt-5">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                  <th>Password</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users?.map((user, index) => {
+                  return (
+                    <tr key={user.id}>
+                      <td>{index + 1}</td>
+                      <td>{user.firstName}</td>
+                      <td>{user.lastName}</td>
+                      <td>{user.username}</td>
+                      <td>{user.password}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          )}
+        </main>
+      </Container>
     </>
   );
 }
