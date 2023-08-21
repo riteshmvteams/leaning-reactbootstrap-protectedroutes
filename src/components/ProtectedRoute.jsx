0fb97fc3fc292980ac6c639921c07c2a426d37d3
Ritesh <ritesh.mvteams@gmail.com>
2023-08-21 12:10:34 +0530

    remove comment

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,5 @@
 import { Navigate } from "react-router-dom";
 
-// type Props = {
-//   isLogged: Boolean,
-//   children: React.ReactNode,
-// };
-
 export default function ProtectedRoute({ isLogged, children }) {
   if (!isLogged) {
     return <Navigate to="/" replace />;
